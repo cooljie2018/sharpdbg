44f54cdfe22d831d850e027f33a1b43772cfdcf5
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2026-07-03 18:29:14 +1000

    remove resharper test runner set env vars

--- tests/SharpDbg.Cli.Tests/Helpers/DebuggableProcessHelper.cs
+++ tests/SharpDbg.Cli.Tests/Helpers/DebuggableProcessHelper.cs
@@ -6,7 +6,6 @@ public static class DebuggableProcessHelper
 {
 	public static Process StartDebuggableProcess(bool startSuspended = false)
 	{
-		var useShellExecute = !startSuspended;
 		var filePath = Path.JoinFromGitRoot("artifacts", "bin", "DebuggableConsoleApp", "debug", OperatingSystem.IsWindows() ? "DebuggableConsoleApp.exe" : "DebuggableConsoleApp");
 		if (File.Exists(filePath) is false) throw new FileNotFoundException("DebuggableConsoleApp executable not found", filePath);
 		var process = new Process
@@ -16,11 +15,17 @@ public static class DebuggableProcessHelper
 				FileName = filePath,
 				RedirectStandardInput = false,
 				RedirectStandardOutput = false,
-				UseShellExecute = useShellExecute,
+				UseShellExecute = false,
 				CreateNoWindow = false
 			}
 		};
-		if (startSuspended) process.StartInfo.EnvironmentVariables["DOTNET_DefaultDiagnosticPortSuspend"] = "1";
+		if (startSuspended) process.StartInfo.Environment["DOTNET_DefaultDiagnosticPortSuspend"] = "1";
+
+		// Rider/Resharper Test runner adds these env vars to the launched test process when debugging the test, which would get inherited by the debuggee process that we launch.
+		// This can cause behaviour changes in tests, e.g. the stack frames that are returned (caused by COMPLUS_ReadyToRun, seems like some LINQ is shipped R2R)
+		// Remove them.
+		List<string> envVarsToRemove = ["COMPLUS_FORCEENC", "COMPLUS_ReadyToRun", "COMPLUS_ZapDisable", "DOTNET_GCConserveMemory", "DOTNET_GCHeapCount", "DOTNET_GCNoAffinitize", "DOTNET_MODIFIABLE_ASSEMBLIES", "DOTNET_MULTILEVEL_LOOKUP", "DOTNET_TieredPGO", "DOTNET_gcServer", "_NO_DEBUG_HEAP"];
+		foreach (var envVar in envVarsToRemove) process.StartInfo.Environment.Remove(envVar);
 
 		process.Start();
 		return process;
